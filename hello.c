95b59d52bb427183acf996d93a3a19770cc8d906
Acarrillo-7 <acarrillo7@unl.edu>
2024-08-26 22:34:19 -0500

    program

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Andrew Carrillo
+ *         acarrillo7@unl.edu
+ * Date: 08/26/2024
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andrew Carrillo, Computer Engineer\n");
+  printf("Hello World! I love the School of Computing\n");
+
+  return 0;
+}
+
